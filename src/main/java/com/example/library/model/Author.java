8456d53e6d571bb17e3b7373f72243e1f7251ea5
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-08-12 21:55:26 +0200

    Library Create Author

--- src/main/java/com/example/library/model/Author.java
+++ src/main/java/com/example/library/model/Author.java
@@ -1,4 +1,16 @@
 package com.example.library.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String categories;
 }
